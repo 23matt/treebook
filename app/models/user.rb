dfd1310753f09297239329376fc1175c7138efd1
Matthias Gisler <matthias@gisler.me>
2014-02-25 22:47:57 +0100

    Styled the flash messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 	 												 uniqueness: true,
 	 												 format: {
 	 												 	with: /a-zA-Z0-9_-/,
-	 												 	message: 'Must be formatted correctly.'
+	 												 	message: 'must be formatted correctly.'
 	 												 }
   
   has_many :statuses
